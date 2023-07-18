ceded9cc01ca492cd6f3d4758eed8837862b22fa
jryu20 <111260259+jryu20@users.noreply.github.com>
2023-07-18 13:01:39 -0700

    new files

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
 from flask import redirect, url_for, abort
 import sqlite3
+import numpy as np
 
 app = Flask(__name__)
 
 def get_message_db():
-  # write some helpful comments here
     try:
         return g.message_db
     except:
@@ -16,7 +16,36 @@ def get_message_db():
         return g.message_db
 
 def insert_messages(request):
+    db = get_message_db()
+    cursor = db.cursor()
+    cmd = 'SELECT COUNT(*) FROM messages'
+    new_id = cursor.execute(cmd).fetchone()[0] + 1
     
+    cmd = f"INSERT INTO messages VALUES ({new_id}, '{request.form['handle']}', '{request.form['message']}')"
+    cursor.execute(cmd)
+    db.commit()
+    db.close()
     
+@app.route("/", methods=['POST', 'GET'])
+def render():
+    if request.method == 'GET':
+        return render_template('submit.html')
+    else:
+        insert_message(request.form)
+        return render_template('submit.html', message=message, handle=handle)
+
+def random_messages(n):
+    db = get_message_db()
+    cursor = db.cursor()
+    cmd = f"SELECT * FROM messages ORDER BY RANDOM() LIMIT {n}"
+    messages = cursor.execute(cmd).fetchall()
+    db.close()
+
+    return messages
+
+@app.route('/view/', methods=['POST', 'GET'])
+def view():
+    messages = random_submissions(np.random.randint(1,6))
+    return render_template('view.html', messages=messages)
 
 
